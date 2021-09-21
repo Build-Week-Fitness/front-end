807ca1fc1fc7a41839d97ea562dd60a40664aff1
Amy Lam <amy@Marcos-MBP.hsd1.ca.comcast.net>
2021-09-20 22:47:28 -0700

    added styling on Login & SignUp pages

--- fitness-app/src/components/Registration.js
+++ fitness-app/src/components/Registration.js
@@ -1,28 +1,41 @@
+import { Link } from 'react-router-dom';
+
 export default function Registration() {
   return (
-    <>
-      <h1>Registration</h1>
-      <form>
-        <label>
-          Create Username:
-          <input
-            type="text"
-            name="username"
-            placeholder="Create Username"
-            max-characters="14"
-          />
-          <br />
-          Create Password:
-          <input
-            type="text"
-            name="password"
-            placeholder="Create Password"
-            max-characters="14"
-          />
-        </label>
-        <br />
-        <button>Sign Up</button>
-      </form>
-    </>
+    <div className="form-wrapper">
+      <div className="form-text-container">
+        <h2>Create Your Account</h2>
+        <p>Already have an account?
+          <Link to="/login">
+            <button type='button' className='switch'>
+              Login
+            </button>
+          </Link>
+        </p>
+      </div>
+      <div className="form-container">
+        <form className="login-form">
+          <label>
+            Username
+            <input
+              type="text"
+              name="username"
+              placeholder="Enter Username"
+              max-characters="14"
+            />
+          </label>
+          <label>
+            Password
+            <input
+              type="text"
+              name="password"
+              placeholder="Enter Password"
+              max-characters="14"
+            />
+          </label>
+          <button id="submit-login" type="submit">Sign Up</button>
+        </form>
+      </div>
+    </div>
   );
 }
--- fitness-app/src/components/login.js
+++ fitness-app/src/components/login.js
@@ -1,36 +1,41 @@
 import { Link } from 'react-router-dom';
+
 export default function Login() {
   return (
-    <>
-      <h2>Login to your account</h2>
-      <p>Do not have an account?
-        <Link to="/register">
-          <button type='button' className='switch'>
-            Create account
-          </button>
-        </Link>
-      </p>
-      <form>
-        <label>
-          Username:
-          <input
-            type="text"
-            name="username"
-            placeholder="Enter Username"
-            max-characters="14"
-          />
-          <br />
-          Password:
-          <input
-            type="text"
-            name="password"
-            placeholder="Enter Password"
-            max-characters="14"
-          />
-        </label>
-        <br />
-        <button>Log In</button>
-      </form>
-    </>
+    <div className="form-wrapper">
+      <div className="form-text-container">
+        <h2>Login to your account</h2>
+        <p>Do not have an account?
+          <Link to="/register">
+            <button type='button' className='switch'>
+              Create account
+            </button>
+          </Link>
+        </p>
+      </div>
+      <div className="form-container">
+        <form className="login-form">
+          <label>
+            Username:
+            <input
+              type="text"
+              name="username"
+              placeholder="Enter Username"
+              max-characters="14"
+            />
+          </label>
+          <label>
+            Password:
+            <input
+              type="text"
+              name="password"
+              placeholder="Enter Password"
+              max-characters="14"
+            />
+          </label>
+          <button id="submit-login" type="submit">Log In</button>
+        </form>
+      </div>
+    </div>
   );
 }
