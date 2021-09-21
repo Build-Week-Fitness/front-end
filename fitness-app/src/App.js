59fa58c01f83264334dba9eacadede1fdc184d33
Amy Lam <amy@Marcos-MBP.hsd1.ca.comcast.net>
2021-09-20 22:13:54 -0700

    applied styling for nav bar

--- fitness-app/src/App.js
+++ fitness-app/src/App.js
@@ -1,11 +1,35 @@
 import "./App.css";
 import Home from "./components/Home";
-import { Link, Route } from "react-router-dom";
+import Login from "./components/login";
+import { Link, Route, Switch } from "react-router-dom";
 import Registration from "./components/Registration";
 
 function App() {
   return (
     <div className="App">
+      <header className="App-header">
+        <h1>Everywhere Fitness</h1>
+        <nav>
+          <div className="nav-links">
+            <Link to="/">Home</Link>
+            <Link to="/login">Login</Link>
+          </div>
+        </nav>
+      </header>
+      <Switch>
+
+        <Route path="/login">
+          <Login />
+        </Route>
+
+        <Route path="/register">
+          <Registration />
+        </Route>
+
+        <Route path="/">
+          <Home />
+        </Route>
+      </Switch>
       <Home />
     </div>
   );
